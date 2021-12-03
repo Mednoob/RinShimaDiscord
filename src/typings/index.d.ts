37a97ea2cf593e78ec334f4ce55211c52c692f15
Mednoob <ahmadyaqdhan123@gmail.com>
2021-12-03 18:00:11 +0900

    types: add osu raw datas

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -45,3 +45,40 @@ export interface TextCommandQuery extends BaseCommandQuery {
 }
 
 export type CommandQuery = TextCommandQuery;
+export type OsuRank = "XH" | "X" | "SH" | "S" | "A" | "B" | "C" | "D" | "F";
+
+interface BaseOsuData {
+    user_id: string;
+    count300: string;
+    count100: string;
+    count50: string;
+    countmiss: string;
+    countgeki: string;
+    countkatu: string;
+}
+
+interface BaseGameplay extends BaseOsuData {
+    beatmap_id: string;
+    score_id: string;
+    maxcombo: string;
+    rank: OsuRank;
+    perfect: "0"|"1";
+    enabled_mods: string;
+    date: string;
+    score: string;
+    replay_available: "0"|"1";
+    pp: string;
+}
+
+export interface RawOsuScore extends BaseGameplay {
+    beatmap_id: never;
+    username: string;
+}
+
+export interface RawUserRecent extends BaseGameplay {
+    score_id: never;
+    replay_available: never;
+    pp: never;
+}
+
+export type RawUserBest = BaseGameplay;
