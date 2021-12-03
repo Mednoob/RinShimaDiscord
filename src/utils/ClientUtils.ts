562fcdacac24e416747a56f45d7420ae836a1644
Mednoob <ahmadyaqdhan123@gmail.com>
2021-12-03 16:19:44 +0900

    feat(ClientUtils): add 'createEmbed' util

--- src/utils/ClientUtils.ts
+++ src/utils/ClientUtils.ts
@@ -1,6 +1,25 @@
+import { ColorResolvable, MessageEmbed } from "discord.js";
 import got from "got";
 
+type embedColorType = "info" | "success" | "warning" | "danger";
+const embedColors: Record<embedColorType, string> = {
+    info: "3CAAFF",
+    success: "GREEN",
+    warning: "YELLOW",
+    danger: "RED"
+};
+
+function createEmbed(type: embedColorType, message?: string): MessageEmbed {
+    const embed = new MessageEmbed()
+        .setColor(embedColors[type] as ColorResolvable);
+
+    if (message) embed.setDescription(message);
+    return embed;
+}
+
 export class ClientUtils {
+    public static readonly createEmbed = createEmbed;
     public static readonly REST = got;
+    public readonly createEmbed = createEmbed;
     public readonly REST = got;
 }
