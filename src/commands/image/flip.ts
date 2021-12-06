f77e57a72f761b4f9acc16a0cec927d87b60b1ef
Mednoob <ahmadyaqdhan123@gmail.com>
2021-12-06 19:35:51 +0900

    feat(commands/image): create flip command

---
+++ src/commands/image/flip.ts
@@ -0,0 +1,45 @@
+import { BaseCommand } from "../../structures/BaseCommand";
+import { CommandQueryContext } from "../../structures/CommandQueryContext";
+import { MessageAttachment } from "discord.js";
+import sharp from "sharp";
+
+export default class FlipCommand extends BaseCommand {
+    public constructor(rin: BaseCommand["rin"]) {
+        super(rin, {
+            query: "flip",
+            type: "text"
+        });
+    }
+
+    public async execute(ctx: CommandQueryContext): Promise<void> {
+        const url = (ctx.isMessage() ? ctx.context.attachments.first()?.url : undefined) ?? (this.rin.utils.isUrl(ctx.args[0]) ? ctx.args[0] : undefined) ?? (this.rin.users.cache.get(ctx.args[0]?.replace(/[^0-9]/g, "")) ?? (ctx.isMessage() ? ctx.context.member! : (ctx.isInteraction() ? ctx.context.user : undefined)))?.displayAvatarURL({ size: 2048, format: "png" });
+        const buff = await this.rin.utils.REST.get(url ?? "").buffer().catch(() => undefined);
+        if (!buff) {
+            void ctx.reply({
+                embeds: [
+                    this.rin.utils.createEmbed("danger", "Invalid image URL")
+                ]
+            });
+            return;
+        }
+
+        try {
+            const edited = await sharp(buff)
+                .rotate(180)
+                .png()
+                .toBuffer();
+
+            void ctx.reply({
+                files: [
+                    new MessageAttachment(edited, "flip.png")
+                ]
+            });
+        } catch {
+            void ctx.reply({
+                embeds: [
+                    this.rin.utils.createEmbed("danger", "Invalid image data")
+                ]
+            });
+        }
+    }
+}
