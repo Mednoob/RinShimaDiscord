f296269e0211c8f0ae222ea6200b6f6b109b1a03
Mednoob <ahmadyaqdhan123@gmail.com>
2021-12-11 00:25:11 +0900

    feat(utils): add MessageCheckerManager

---
+++ src/utils/MessageCheckerManager.ts
@@ -0,0 +1,23 @@
+import { Rin } from "../structures/Rin";
+import { Message } from "discord.js";
+import { readdirSync } from "fs";
+import { resolve } from "path";
+
+export type MessageChecker = (message: Message) => void;
+
+export class MessageCheckerManager {
+    public readonly checkers: MessageChecker[] = [];
+
+    public constructor(public readonly rin: Rin, private readonly basePath: string) {}
+
+    public async load(): Promise<void> {
+        const files = readdirSync(this.basePath);
+
+        for (const file of files) {
+            const fileData = await import(resolve(this.basePath, file));
+            if (!fileData.default) continue;
+
+            this.checkers.push(fileData.default as MessageChecker);
+        }
+    }
+}
