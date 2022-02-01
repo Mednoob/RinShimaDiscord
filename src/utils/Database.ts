568cd12d567eff38d92748f0cf3ca67b65f20c73
Mednoob <ahmadyaqdhan123@gmail.com>
2022-02-01 15:33:43 +0900

    feat: add database connector

---
+++ src/utils/Database.ts
@@ -0,0 +1,23 @@
+import { createConnection, Connection } from "typeorm";
+import { resolve } from "path";
+
+export class Database<Ready extends boolean = false> {
+    private connection!: Ready extends true ? Connection : (Connection|undefined);
+
+    public async connect(): Promise<Database<true>> {
+        this.connection = await createConnection({
+            entities: [
+                resolve(__dirname, "..", "entities", "*.{ts,js}")
+            ],
+            logging: "all",
+            type: "mongodb",
+            useUnifiedTopology: true
+        });
+
+        return this as Database<true>;
+    }
+
+    public isConnected(): this is Database<true> {
+        return this.connection !== undefined;
+    }
+}
