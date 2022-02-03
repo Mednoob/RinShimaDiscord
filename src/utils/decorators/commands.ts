846173d5d39dace0ef1085ca85f4d732ca71dc9c
Mednoob <ahmadyaqdhan123@gmail.com>
2022-02-03 21:33:48 +0900

    feat(decorators/commands): make a re-usable decorator creator

--- src/utils/decorators/commands.ts
+++ src/utils/decorators/commands.ts
@@ -1,21 +1,33 @@
 import { BaseCommand } from "../../structures/BaseCommand";
-import { ClassDecorator, CommandQuery, MethodDecorator } from "../../typings";
+import { ClassDecorator, CommandQuery, MethodDecorator, Promisable } from "../../typings";
 import { PermissionString } from "discord.js";
 
-export function requirePermissions(permissions: PermissionString[]): MethodDecorator<BaseCommand, void> {
+function createQueryExecutionDecorator(
+    func: (query: BaseCommand, ...args: Parameters<BaseCommand["execute"]>) => Promisable<boolean | undefined>
+): MethodDecorator<BaseCommand, void> {
     return (target, _, descriptor) => {
         const originalMethod = target.execute.bind(target);
 
-        descriptor.value = function value(context: Parameters<BaseCommand["execute"]>[0]) {
-            if (!context.member?.permissions.has(permissions)) {
-                return context.reply({
-                    content: "You don't have the required permissions to use this command."
-                });
-            }
+        descriptor.value = async function value(...args: Parameters<BaseCommand["execute"]>) {
+            const res = await func(target, ...args);
+            if (res === false) return;
 
-            return originalMethod.apply(this, [context]);
+            return originalMethod(...args);
         };
     };
+}
+
+export function requirePermissions(permissions: PermissionString[]): MethodDecorator<BaseCommand, void> {
+    return createQueryExecutionDecorator((query, ctx) => {
+        if (!ctx.member?.permissions.has(permissions)) {
+            void ctx.reply({
+                embeds: [
+                    query.rin.utils.createEmbed("danger", "You don't have the required permissions to execute this command.")
+                ]
+            });
+            return false;
+        }
+    });
 }
 
 type ExtendedCommandConstructor = new (...args: ConstructorParameters<typeof BaseCommand>) => BaseCommand;
